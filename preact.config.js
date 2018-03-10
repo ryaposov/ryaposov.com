63f83f48ea17aa2f60d9616c6c7651b6465ac5f1
Pavel <ryaposov@me.com>
2018-03-10 16:42:01 +0300

    0.4 - Build configuration

--- preact.config.js
+++ preact.config.js
@@ -10,7 +10,24 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default (config, env, helpers) => {
+	// Copy all contents of assets folder
 	config.plugins.push(
 		new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: '**/*' }])
 	);
+
+	// Configure babel
+	let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
+	rule.options.plugins.push('transform-regenerator');
+	rule.options.plugins.push(['transform-runtime', {
+		'helpers': false,
+		'polyfill': false,
+		'regenerator': true
+	}]);
+
+	// All recipes here: https://github.com/developit/preact-cli/wiki/Config-Recipes
+	// Disable sourcemaps
+	let uglify = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+	if (uglify && uglify.plugin) {
+		uglify.plugin.options.sourceMap = false;
+	}
 };
