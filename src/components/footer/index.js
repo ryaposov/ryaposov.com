f1f324097e2450fe9b4c648827f302c7b7aad2c3
Pavel <ryaposov@me.com>
2018-11-27 11:21:11 +0100

    Analytics integration 2

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -24,7 +24,6 @@ const Footer = () => (
 			</nav>
 			<span class={style.footer__email}>{footer.email}</span>
 		</div>
-		<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter51317566 = new Ya.Metrika2({ id:51317566, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/51317566" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 	</footer>
 );
 
